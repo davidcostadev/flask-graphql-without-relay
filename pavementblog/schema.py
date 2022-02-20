1173d428762f36ef0acf7d4bbba19bfeff565c0c
David Costa <davidcostadev@gmail.com>
2022-02-20 13:42:14 -0300

    Adding sorting

--- pavementblog/schema.py
+++ pavementblog/schema.py
@@ -35,11 +35,22 @@ class FilterIdNumber(graphene.InputObjectType):
 
 class PaginationInput(graphene.InputObjectType):
     page = graphene.Int(default_value=1)
-    limit = graphene.Int(default_value=10)
+    limit = graphene.Int(default_value=25)
 
 
 class ArticleFilter(graphene.InputObjectType):
     id = FilterIdNumber()
+
+
+class Sorting(graphene.Enum):
+    ASC = "asc"
+    DESC = "desc"
+
+
+class ArticleSorting(graphene.InputObjectType):
+    id = Sorting()
+    title = Sorting()
+    published_at = Sorting()
 
 
 class Query(graphene.ObjectType):
@@ -49,37 +60,67 @@ class Query(graphene.ObjectType):
 
     articles = graphene.Field(ArticleListResult,
                               filter=ArticleFilter(),
-                              pagination=PaginationInput())
+                              pagination=PaginationInput(),
+                              sorting=ArticleSorting())
 
-    def resolve_articles(self, info, filter=None, pagination=None):
+    def resolve_articles(self,
+                         info,
+                         filter=None,
+                         pagination=None,
+                         sorting=None):
         query = Article.get_query(info)
-
         query = filters(query, filter)
+        query = sortingBy(query, sorting)
 
         return list_result(query, pagination)
 
 
+def sortingBy(query, sorting):
+    if sorting:
+        if sorting.id:
+            if sorting.id == 'asc':
+                query = query.order_by(ArticleModel.id.asc())
+            else:
+                query = query.order_by(ArticleModel.id.desc())
+        if sorting.published_at:
+            if sorting.published_at == 'asc':
+                query = query.order_by(ArticleModel.published_at.asc())
+            else:
+                query = query.order_by(ArticleModel.published_at.desc())
+        if sorting.title:
+            if sorting.title == 'asc':
+                query = query.order_by(ArticleModel.title.asc())
+            else:
+                query = query.order_by(ArticleModel.title.desc())
+
+    return query
+
+
 def filters(query, filter):
-    if (filter):
+    if filter:
         query = filter_id(query, filter)
 
     return query
 
 
 def filter_id(query, filter):
-    if (filter.id):
-        if (filter.id.eq):
+    if filter.id:
+        if filter.id.eq:
             query = query.filter(ArticleModel.id == filter.id.eq)
 
-        if (filter.id.in_list):
+        if filter.id.in_list:
             query = query.filter(ArticleModel.id.in_(filter.id.in_list))
 
     return query
 
 
 def list_result(query, pagination=None):
-    page = pagination.get('page', 1)
-    limit = pagination.get('limit', 25)
+    if pagination:
+        page = pagination.get('page', 1)
+        limit = pagination.get('limit', 25)
+    else:
+        page = 1
+        limit = 25
 
     totalItems = query.count()
     totalPages = math.ceil(totalItems / limit)
